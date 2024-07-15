fd8e32022080b5fd072c84e7f87557325b51ded3
Md Musfikur Rahman <107528767+Md-Musfikur-Rahman@users.noreply.github.com>
2024-07-15 13:37:59 +0600

    last try

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div className="container">
-      <div className="text-3xl text-black">
+      <div className=" text-center text-3xl text-black">
         <h1>Website</h1>
         <h2>Under Construction</h2>
       </div>
