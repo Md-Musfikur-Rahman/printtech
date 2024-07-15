f7f301f10a29eb1870170448c2a1c5b719eca104
Md Musfikur Rahman <107528767+Md-Musfikur-Rahman@users.noreply.github.com>
2024-07-15 13:26:41 +0600

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,10 @@
-import Head from "next/head";
-
 export default function HomePage() {
   return (
-    <>
-      <Head>
-        <title>Website Under Construction</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-      </Head>
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-10 main-title">
-            <h1>Website</h1>
-            <h2>Under Construction</h2>
-          </div>
-        </div>
+    <div className="container">
+      <div className="text-3xl text-black">
+        <h1>Website</h1>
+        <h2>Under Construction</h2>
       </div>
-    </>
+    </div>
   );
 }
